d4af5b29d7a95a2ef8b8bbb4654d446e09a3d78c
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-07-03 12:10:13 +0300

    add docheader

--- tests/EnqueueMessageProducerTest.php
+++ tests/EnqueueMessageProducerTest.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * This file is part of the prooph/psb-enqueue-producer.
+ * (c) 2016-2017 prooph software GmbH <contact@prooph.de>
+ * (c) 2016-2017 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ * (c) 2017 Maksym Kotliar <kotlyar.maksim@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace ProophTest\ServiceBus\Functional;
